d65e9a820bb472c295cb385206bdf3dc503e7766
shinobe179 <befs.anne@gmail.com>
2024-08-26 02:02:26 +0900

    3章まで完了

---
+++ app/_components/Date/index.tsx
@@ -0,0 +1,15 @@
+import styles from "./index.module.css";
+import Image from "next/image";
+
+type Props = {
+  date: string;
+};
+
+export default function Date({ date }: Props) {
+  return (
+    <span className={styles.date}>
+      <Image src="/clock.svg" alt="" width={16} height={16} loading="eager" />
+      {date}
+    </span>
+  );
+}
