e33b58019cc022bad4227bb848998e9d5b2b1b75
shinobe179 <befs.anne@gmail.com>
2024-08-27 01:54:38 +0900

    4-4-1

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./index.module.css";
+import Menu from "@/app/_components/Menu";
 
 export default function Header() {
   return (
@@ -15,19 +16,7 @@ export default function Header() {
           priority
         />
       </Link>
-      <nav className={styles.nav}>
-        <ul className={styles.items}>
-          <li>
-            <Link href="/news">ニュース</Link>
-          </li>
-          <li>
-            <Link href="/members">メンバー</Link>
-          </li>
-          <li>
-            <Link href="/contact">コンタクト</Link>
-          </li>
-        </ul>
-      </nav>
+      <Menu />
     </header>
   );
 }
---
+++ app/_components/Menu/index.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./index.module.css";
+
+export default function Menu() {
+  return (
+    <div>
+      {" "}
+      <nav className={styles.nav}>
+        <ul className={styles.items}>
+          <li>
+            <Link href="/news">ニュース</Link>
+          </li>
+          <li>
+            <Link href="/members">メンバー</Link>
+          </li>
+          <li>
+            <Link href="/contact">コンタクト</Link>
+          </li>
+        </ul>
+      </nav>
+      <button className={styles.button}>
+        <Image src="/menu.svg" alt="メニュー" width={24} height={24} />
+      </button>
+    </div>
+  );
+}
