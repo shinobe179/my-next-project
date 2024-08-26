5ae57f96ee0ef85469051531bbde210d058c4686
shinobe179 <befs.anne@gmail.com>
2024-08-27 00:30:11 +0900

    4-2-2

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./index.module.css";
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <a href="/" className={styles.logoLink}>
+      <Link href="/" className={styles.logoLink}>
         <Image
           src="/logo.svg"
           alt="SIMPLE"
@@ -13,11 +14,11 @@ export default function Header() {
           height={133}
           priority
         />
-      </a>
+      </Link>
       <nav>
         <ul className={styles.items}>
           <li>
-            <a href="/members">メンバー</a>
+            <Link href="/members">メンバー</Link>
           </li>
         </ul>
       </nav>
